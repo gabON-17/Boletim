5d2bb457a1f716dbc27aefc987e8e96c7ccb6f55
gabbs <gabrielbarros1704@gmail.com>
2025-02-04 20:09:40 -0300

    Tudo certo

--- Boletim-Escolar-2.0.py
+++ Boletim-Escolar-2.0.py
@@ -1,22 +1,21 @@
 from func import mysql, dados, interface
 #  Ler as matérias do usuário e mostrar o boletim do aluno
 cont_id_mat = 0
-tot_notas = 0
 notas = list()
-alunos_id = list()
 
 interface.titulo('meu boletim')
 
 aluno = dados.valida_str('Digite seu nome: ') # Pega o nome do aluno 
-# mysql.com_sql(f'insert into alunos (nome) values ("{aluno}")') # Registra o aluno no banco
+mysql.com_sql(f'insert into alunos values (default, "{aluno}")') # Registra o aluno no banco
 
 materias = mysql.tabela('select nome from materias') # Tem todas as matérias presente no banco de dados
-id_aluno = mysql.tabela('select id_aluno from alunos')
 id_mat = mysql.tabela('select id_materia from materias')
+id_aluno = mysql.tabela('select id_aluno from alunos')
 
 interface.subTitulo('Digite sua notas')
 
 for valor in materias:
+        tot_notas = 0
         for c in range(0, 4):
             nota = dados.valida_nota(f'{c+1} Bimestre em {valor}: ')
             notas.append(nota)
@@ -25,9 +24,8 @@ for valor in materias:
 
             if c == 3:
                 notas.append(tot_notas)
-                print(notas)
-                mysql.com_sql(f'insert into notas (id_aluno, id_materia, 1_Bimestre, 2_Bimestre, 3_Bimestre, 4_Bimestre, total) values ("{id_aluno[-1]}", "{id_mat[cont_id_mat]}", "{notas[0]}", "{notas[1]}", {notas[2]}, "{notas[3]}", "{notas[4]}" )')
+                mysql.com_sql(f'insert into notas values (default, "{id_aluno[-1]}", "{id_mat[cont_id_mat]}", "{notas[0]}", "{notas[1]}", {notas[2]}, "{notas[3]}", "{notas[4]}" )')
                 cont_id_mat += 1
                 notas.clear()
 
-interface.linha(15)
+        interface.linha(15)
