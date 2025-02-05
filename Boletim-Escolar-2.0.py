461c3793cfa4773b432b675fed16be87c24957b4
gabbs <gabrielbarros1704@gmail.com>
2025-02-05 12:24:42 -0300

    Projeto Final

--- Boletim-Escolar-2.0.py
+++ Boletim-Escolar-2.0.py
@@ -7,7 +7,7 @@ notas = list()
 interface.titulo('meu boletim')
 
 aluno = dados.valida_str('Digite seu nome: ') # Pega o nome do aluno 
-# mysql.com_sql(f'insert into alunos values (default, "{aluno}")') # Registra o aluno no banco
+mysql.com_sql(f'insert into alunos values (default, "{aluno}")') # Registra o aluno no banco
 
 id_aluno = mysql.tabela('select id_aluno from alunos')
 interface.subTitulo(f'Seu ID é {id_aluno[-1]}')
@@ -18,21 +18,21 @@ id_mat = mysql.tabela('select id_materia from materias')
 
 interface.subTitulo('Digite sua notas')
 
-# for valor in materias:
-        # tot_notas = 0
-        # for c in range(0, 4):
-            # nota = dados.valida_nota(f'{c+1} Bimestre em {valor}: ')
-            # notas.append(nota)
-# 
-            # tot_notas += nota
+for valor in materias:
+        tot_notas = 0
+        for c in range(0, 4):
+            nota = dados.valida_nota(f'{c+1} Bimestre em {valor}: ')
+            notas.append(nota)
 
-            # if c == 3:
-                # notas.append(tot_notas)
-                # mysql.com_sql(f'insert into notas values (default, "{id_aluno[-1]}", "{id_mat[cont_id_mat]}", "{notas[0]}", "{notas[1]}", {notas[2]}, "{notas[3]}", "{notas[4]}" )')
-                # cont_id_mat += 1
-                # notas.clear()
-# 
-        # interface.linha(15)
+            tot_notas += nota
+
+            if c == 3:
+                notas.append(tot_notas)
+                mysql.com_sql(f'insert into notas values (default, "{id_aluno[-1]}", "{id_mat[cont_id_mat]}", "{notas[0]}", "{notas[1]}", {notas[2]}, "{notas[3]}", "{notas[4]}" )')
+                cont_id_mat += 1
+                notas.clear()
+
+        interface.linha(15)
 
 
 while True:
